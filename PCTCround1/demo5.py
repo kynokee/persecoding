8825cf8c3a8bf0971e962cb15fc58af708e35631
kynokee <125532446+kynokee@users.noreply.github.com>
2023-03-06 10:16:11 +0800

    Create demo5.py

---
+++ PCTCround1/demo5.py
@@ -0,0 +1,10 @@
+firstnum = int(input())
+operator = input()
+secondnum = int(input())
+
+if operator == "+":
+    print(firstnum + secondnum)
+elif operator == "*":
+    print(firstnum * secondnum)
+else:
+    print("wat")
